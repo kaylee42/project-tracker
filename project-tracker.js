a369f810ad377e084245ef809f1227b2f3f6739c
kaylee42 <kayleeknowles42@gmail.com>
2016-03-07 14:34:28 -0500

    ports over work from mood tracker and changes names

--- project-tracker.js
+++ project-tracker.js
@@ -1,23 +1,86 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+Tasks = new Mongo.Collection("tasks");
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+if (Meteor.isClient){
+   Template.body.helpers({
+     tasks: function(){
+       return Tasks.find({}, {sort: {createdAt: -1}});
+     }
+   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+   Template.body.events({
+     "submit #new-task-form": function(event){
+       event.preventDefault();
+       var task = event.target.task.value;
+       var notes = event.target.notes.value;
+
+       Tasks.insert({
+         task: task,
+         notes: notes,
+         createdAt: new Date()
+       });
+
+       document.getElementById("new-task-form").reset();
+
+     }
+   });
+   Template.task.events({
+     "dblclick .editable": function(event, target){
+       return Session.set("target" + target.data._id, true);
+     },
+     "dblclick .editable2": function(event, target){
+       return Session.set("target2" + target.data._id, true);
+     },
+     "keypress .savable": function(event, target){
+       if(event.keyCode == 13){
+         Tasks.update(this._id, {
+           $set: {task: event.currentTarget.value}
+         });
+         return Session.set("target" + target.data._id, false);
+       }
+     },
+     "keypress .savable2": function(event, target){
+       if(event.keyCode == 13){
+         Tasks.update(this._id, {
+           $set: {notes: event.currentTarget.value}
+         });
+         return Session.set("target2" + target.data._id, false);
+       }
+     }
+   });
+
+   Template.task.helpers({
+     formatDate: function(date){
+       return `${date.getMonth()}/${date.getDate()}/${date.getFullYear()}`;
+     },
+     editing: function(){
+       return Session.get("target" + this._id);
+     },
+     editing2: function(){
+       return Session.get("target2" + this._id);
+     }
+   });
 }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
+// if (Meteor.isClient) {
+//   // counter starts at 0
+//   Session.setDefault('counter', 0);
+//
+//   Template.hello.helpers({
+//     counter: function () {
+//       return Session.get('counter');
+//     }
+//   });
+//
+//   Template.hello.events({
+//     'click button': function () {
+//       // increment the counter when button is clicked
+//       Session.set('counter', Session.get('counter') + 1);
+//     }
+//   });
+// }
+//
+// if (Meteor.isServer) {
+//   Meteor.startup(function () {
+//     // code to run on server at startup
+//   });
+// }
